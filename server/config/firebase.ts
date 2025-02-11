22b689b7a3d48057a7822f4cf37f82e9050674f0
Morfusee <markvalenzuela.coding@gmail.com>
2025-02-11 17:51:54 +0800

    feat: init firebase

---
+++ server/config/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA80dTz8MbQCNFoQ-DiWEWvElUUwcLlEk8",
+  authDomain: "fir-erd-af873.firebaseapp.com",
+  projectId: "fir-erd-af873",
+  storageBucket: "fir-erd-af873.firebasestorage.app",
+  messagingSenderId: "433005157423",
+  appId: "1:433005157423:web:817c632fd9a71f08ddbada",
+  measurementId: "G-VDSYEWHNPZ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
