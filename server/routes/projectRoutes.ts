3293c00b2e4ccd448a9ebe33dec033c24b1d55af
Morfusee <markvalenzuela.coding@gmail.com>
2025-02-11 19:06:15 +0800

    feat: implemented token verification for projects route

--- server/routes/projectRoutes.ts
+++ server/routes/projectRoutes.ts
@@ -24,7 +24,6 @@ import {
 } from "@root/middleware/validators/projectValidator";
 import { verifyToken } from "@root/middleware/validators/authValidator";
 
-
 const router = express.Router();
 
 // Define routes for project-related operations
@@ -45,7 +44,7 @@ router.get("", [verifyToken, validateUserIdQuery, validate], getProjects);
  * - validateProjectId: Ensures the provided project ID is valid.
  * - validate: General validation middleware.
  */
-router.get("/:id", [validateProjectId, validate], getProjectById);
+router.get("/:id", [verifyToken, validateProjectId, validate], getProjectById);
 
 /**
  * PATCH /projects/:id
@@ -60,6 +59,7 @@ router.get("/:id", [validateProjectId, validate], getProjectById);
 router.patch(
   "/:id",
   [
+    verifyToken,
     validateProjectId,
     validateRequestBodyNotEmpty,
     validateDataUpdateRestriction,
@@ -81,7 +81,13 @@ router.patch(
  */
 router.patch(
   "/:id/data",
-  [validateProjectId, validateProjectData, validateOnlyDataField, validate],
+  [
+    verifyToken,
+    validateProjectId,
+    validateProjectData,
+    validateOnlyDataField,
+    validate,
+  ],
   saveProject
 );
 
@@ -95,7 +101,7 @@ router.patch(
  */
 router.post(
   "",
-  [...validateProjectFields, validateUserId, validate],
+  [verifyToken, ...validateProjectFields, validateUserId, validate],
   createProject
 );
 
@@ -106,6 +112,10 @@ router.post(
  * - validateProjectId: Ensures the provided project ID is valid.
  * - validate: General validation middleware.
  */
-router.delete("/:id", [validateProjectId, validate], deleteProject);
+router.delete(
+  "/:id",
+  [verifyToken, validateProjectId, validate],
+  deleteProject
+);
 
 export default router;
