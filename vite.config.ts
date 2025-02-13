2f60028573024987b5388d10656897d92c93613c
Morfusee <markvalenzuela.coding@gmail.com>
2025-02-13 18:58:09 +0800

    test: test security headers

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    headers: {
+      "Strict-Transport-Security": "max-age=86400; includeSubDomains", // Adds HSTS options to your website, with a expiry time of 1 day
+      "X-Content-Type-Options": "nosniff", // Protects from improper scripts runnings
+      "X-Frame-Options": "DENY", // Stops your site being used as an iframe
+      "X-XSS-Protection": "1; mode=block", // Gives XSS protection to legacy browsers
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
