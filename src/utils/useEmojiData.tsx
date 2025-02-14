f7646ccb67912b35de48f329733913397a1b159a
Morfusee <markvalenzuela.coding@gmail.com>
2025-02-14 18:45:49 +0800

    feat: migrated emoji api calls to emojiApi

--- src/utils/useEmojiData.tsx
+++ src/utils/useEmojiData.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { EmojiAsyncGroup, EmojiData } from "../types/EmojiData";
 import { usePrevious } from "@mantine/hooks";
+import { getEmojiGroup } from "../data/api/emojiApi";
 
 const useEmojiData = (group: keyof EmojiAsyncGroup) => {
   // Define the state variables
@@ -12,43 +13,14 @@ const useEmojiData = (group: keyof EmojiAsyncGroup) => {
 
   // Fetch the emoji data when the group changes
   useEffect(() => {
-    fetchEmojiGroup(group);
+    getEmojiGroup(group, {
+      setEmojiData,
+      setLoading,
+      setError,
+    });
   }, [group]);
 
-  const fetchEmojiGroup = async (group: keyof EmojiAsyncGroup) => {
-    try {
-      // Always set loading to true before fetching data
-      setLoading(true);
-
-      // Fetch data from the server
-      const response = await fetch(
-        import.meta.env.VITE_SERVER_URL + `/emojis?group=${group}`
-      );
-
-      // Throw an error if the response is not ok
-      if (!response.ok) {
-        throw new Error("There was an error fetching emojis.");
-      }
-
-      const data: EmojiData[] = await response.json();
-
-      // Set the emoji data to return
-      setEmojiData(data);
-
-      // Reset the error state
-      setError(null);
-    } catch (err) {
-      // Set the error state
-      setError(
-        err instanceof Error ? err.message : "An unknown error occurred."
-      );
-    } finally {
-      // Always set loading to false after fetching data
-      setLoading(false);
-    }
-  };
-
-  return { emojiData, loading, error, fetchEmojiGroup };
+  return { emojiData, loading, error, fetchEmojiGroup: getEmojiGroup };
 };
 
 export default useEmojiData;
