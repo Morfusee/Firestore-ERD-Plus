7743f769588e28be72f80e271d57d69776a7470c
Morfusee <markvalenzuela.coding@gmail.com>
2025-02-14 22:47:33 +0800

    feat: implemented showing of temp cropped image

--- src/components/modals/ManageAccountModal.tsx
+++ src/components/modals/ManageAccountModal.tsx
@@ -1,14 +1,22 @@
-import { Avatar, Box, Button, Group, Stack, Text, TextInput, Title } from "@mantine/core";
-import { ContextModalProps } from "@mantine/modals";
-import { IconTrash, IconUpload } from "@tabler/icons-react";
-import { useEffect } from "react";
-import useUserRepo from "../../data/repo/useUserRepo";
+import {
+  Avatar,
+  Box,
+  Button,
+  FileButton,
+  Group,
+  Stack,
+  Text,
+  TextInput,
+} from "@mantine/core";
 import { useForm } from "@mantine/form";
-
+import { ContextModalProps, modals } from "@mantine/modals";
+import { IconTrash, IconUpload } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
+import { Cropper, CropperRef } from "react-advanced-cropper";
+import useUserRepo from "../../data/repo/useUserRepo";
 
 function ManageAccountModal({ context, id, innerProps }: ContextModalProps) {
-
-  const { user, changeUserDisplayname } = useUserRepo()
+  const { user, changeUserDisplayname } = useUserRepo();
 
   useEffect(() => {
     context.updateModal({
@@ -19,18 +27,24 @@ function ManageAccountModal({ context, id, innerProps }: ContextModalProps) {
         </Text>
       ),
       size: "md",
-      centered: false
+      centered: false,
     });
   }, []);
 
   const form = useForm({
     mode: "uncontrolled",
     initialValues: {
-      name: user?.displayName || "",
+      image: user?.profileImage || "",
+      name: "",
     },
     validate: {
+      image: (value) => {
+        if (!value) {
+          return "Image is required";
+        }
+      },
       name: (value) => {
-        if (!value.trim()) {
+        if (!value) {
           return "Name is required";
         }
       },
@@ -38,46 +52,65 @@ function ManageAccountModal({ context, id, innerProps }: ContextModalProps) {
   });
 
   const handleSubmit = async (values: typeof form.values) => {
-    if(!user || !user.id) return
+    if (!user || !user.id || !values.name) return;
 
     // Add the project to DB and State
-    await changeUserDisplayname(user?.id, values.name)
+    await changeUserDisplayname(user?.id, values.name);
 
     // Reset form and close modal
     form.reset();
-    context.closeModal(id)
+    context.closeModal(id);
   };
 
-  return(
+  const onUpload = (selectedFile: File | null) => {
+    if (!selectedFile) return;
+
+    // Create a URL for the selected file
+    const dataUrl = URL.createObjectURL(selectedFile);
+
+    // Open the crop image modal
+    modals.openContextModal({
+      onClose: () => {
+        // Revoke the object URL to free up memory
+        URL.revokeObjectURL(dataUrl);
+      },
+      modal: "cropImage",
+      innerProps: {
+        image: dataUrl,
+      },
+    });
+  };
+
+  return (
     <Box className="">
-      <form onSubmit={form.onSubmit((values: typeof form.values) => handleSubmit(values))}>
-        <Stack px='md'>
-
+      <form
+        onSubmit={form.onSubmit((values: typeof form.values) =>
+          handleSubmit(values)
+        )}
+      >
+        <Stack px="md">
           <Group ml="lg" align="center" gap="md">
-
-            <Avatar 
-              size={100}
-            />
-
+            <Avatar size={100} src={form.getValues().image} />
             <Group gap="xs">
-              <Button 
-                variant="default"
-                leftSection={<IconUpload size={16}/>}
-              >
-                Upload
-              </Button>
-              <Button 
-                variant="subtle"
-                leftSection={<IconTrash size={16}/>}
-              >
+              <FileButton onChange={onUpload} accept="image/*">
+                {(props) => (
+                  <Button
+                    {...props}
+                    variant="default"
+                    leftSection={<IconUpload size={16} />}
+                  >
+                    Upload
+                  </Button>
+                )}
+              </FileButton>
+              <Button variant="subtle" leftSection={<IconTrash size={16} />}>
                 Remove
               </Button>
             </Group>
-
           </Group>
 
           <Stack gap="sm">
-            <TextInput 
+            <TextInput
               size="md"
               label="Name"
               defaultValue={user?.displayName}
@@ -86,7 +119,7 @@ function ManageAccountModal({ context, id, innerProps }: ContextModalProps) {
               key={form.key("name")}
               {...form.getInputProps("name")}
             />
-            <TextInput 
+            <TextInput
               size="md"
               label="Email"
               value={user?.email}
@@ -97,32 +130,20 @@ function ManageAccountModal({ context, id, innerProps }: ContextModalProps) {
 
           <Group justify="space-between" mt="lg">
             <Group>
-              <Button 
-                variant="default"
-                type="submit"
-              >
+              <Button variant="default" type="submit">
                 Save
               </Button>
-              <Button 
-                variant="light"
-                onClick={()=>context.closeModal(id)}
-              >
+              <Button variant="light" onClick={() => context.closeModal(id)}>
                 Cancel
               </Button>
             </Group>
 
-            <Button 
-              variant="subtle"
-            >
-              Delete Account
-            </Button>
+            <Button variant="subtle">Delete Account</Button>
           </Group>
-
         </Stack>
       </form>
     </Box>
-  )
+  );
 }
 
-
-export default ManageAccountModal
+export default ManageAccountModal;
