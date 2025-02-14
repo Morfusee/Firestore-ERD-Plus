39d7babd8c9d4a6e6b9b6fb5651518f475c22eaf
Morfusee <markvalenzuela.coding@gmail.com>
2025-02-14 18:39:17 +0800

    feat: migrated userApi to using Axios

--- src/data/api/userApi.ts
+++ src/data/api/userApi.ts
@@ -4,54 +4,36 @@ import {
   FetchedUser,
   UpdatedUser,
 } from "../../types/APITypes";
+import axiosInstance from "../../utils/axiosInstance";
 
 export const getUserApi = async (userId: string) => {
-  const response = await fetch(
-    import.meta.env.VITE_SERVER_URL + `/users/${userId}`,
-    {
-      credentials: "include",
-    }
-  )
-    .then((res) => res.json())
+  const response = await axiosInstance
+    .get<APIResponse<FetchedUser>>(`/users/${userId}`)
+    .then((res) => res.data)
     .catch((err) => console.error(err));
 
-  return response as APIResponse<FetchedUser>;
+  return response;
 };
 
 export const updateUserApi = async (userId: string, newDisplayName: string) => {
-  const response = await fetch(
-    import.meta.env.VITE_SERVER_URL + `/users/${userId}`,
-    {
-      method: "PATCH",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        displayName: newDisplayName,
-      }),
-    }
-  )
-    .then((res) => res.json())
+  const response = await axiosInstance
+    .patch<APIResponse<UpdatedUser>>(`/users/${userId}`, {
+      displayName: newDisplayName,
+    })
+    .then((res) => res.data)
     .catch((err) => console.error(err));
 
-  return response as APIResponse<UpdatedUser>;
+  return response;
 };
 
 export const createUserApi = async (username: string, email: string) => {
-  const response = await fetch(import.meta.env.VITE_SERVER_URL + `/users`, {
-    method: "POST",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
+  const response = await axiosInstance
+    .post<APIResponse<CreatedUser>>(`/users/`, {
       username,
       email,
-    }),
-  })
-    .then((res) => res.json())
+    })
+    .then((res) => res.data)
     .catch((err) => console.error(err));
 
-  return response as APIResponse<CreatedUser>;
+  return response;
 };
