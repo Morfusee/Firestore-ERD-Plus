6ea2e5222668b90e2fbb66fdf20251b8d881a30a
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2025-02-22 21:24:53 +0800

    Remove log statements

--- server/middleware/compressImageMiddleware.ts
+++ server/middleware/compressImageMiddleware.ts
@@ -11,9 +11,6 @@ export const compressImage = async (
   }
 
   try {
-    // Log original file details
-    console.log("Original file size (bytes):", req.file.buffer.length);
-
     // Get image metadata
     const originalMetadata = await sharp(req.file.buffer).metadata();
 
@@ -22,17 +19,9 @@ export const compressImage = async (
       throw new Error("Unable to read image dimensions.");
     }
 
-    console.log(
-      "Original file dimensions:",
-      originalMetadata.width,
-      "x",
-      originalMetadata.height
-    );
-
     // Skip compression if the image is already small
     if (req.file.buffer.length <= 10240) {
       // 10KB threshold
-      console.log("Image is already small. Skipping compression.");
       return next();
     }
 
@@ -51,20 +40,12 @@ export const compressImage = async (
       .toFormat("webp", { quality: 60 })
       .toBuffer();
 
-    console.log("Compressed file size (bytes):", compressedImageBuffer.length);
     const compressedMetadata = await sharp(compressedImageBuffer).metadata();
 
     // Check if compressed metadata is valid
     if (!compressedMetadata.width || !compressedMetadata.height) {
       throw new Error("Unable to read compressed image dimensions.");
     }
-
-    console.log(
-      "Compressed file dimensions:",
-      compressedMetadata.width,
-      "x",
-      compressedMetadata.height
-    );
 
     // Replace the original file buffer with the compressed one
     req.file.buffer = compressedImageBuffer;
