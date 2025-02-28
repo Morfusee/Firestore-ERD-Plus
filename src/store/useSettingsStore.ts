a18f68570edde36085905b02d99fac4cb5308a2d
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2025-02-28 23:32:45 +0800

    feat: Add zustand store for managing user settings
    - Define IUserSettings interface to structure user settings
    - Provide setSettings to initialize settings state
    - Implement updateSettings to modify inidividual settings properities dynamically

---
+++ src/store/useSettingsStore.ts
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+import { BackgroundVariant } from "@xyflow/react";
+
+export interface IUserSettings {
+  autoSaveInterval: number;
+  canvasBackground: BackgroundVariant;
+  theme: "System" | "Light" | "Dark";
+}
+
+interface ISettingsState {
+  settings: IUserSettings | null;
+}
+
+interface ISettingsActions {
+  setSettings: (settings: IUserSettings) => void;
+  updateSettings: <K extends keyof IUserSettings>(
+    key: K,
+    value: IUserSettings[K]
+  ) => void;
+}
+
+export const useSettingsStore = create<ISettingsState & ISettingsActions>()(
+  devtools(
+    (set) => ({
+      settings: null,
+
+      setSettings: (settings) => set({ settings }),
+
+      updateSettings: (key, value) =>
+        set((state) => ({
+          settings: state.settings ? { ...state.settings, [key]: value } : null,
+        })),
+    }),
+    { name: "settingsStore" }
+  )
+);
