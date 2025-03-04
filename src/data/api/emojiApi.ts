9727ee8249da5619be4b891336de1eba5b01900c
Morfusee <markvalenzuela.coding@gmail.com>
2025-03-04 09:19:04 +0800

    revert: logging of emoji endpoint fetching

--- src/data/api/emojiApi.ts
+++ src/data/api/emojiApi.ts
@@ -6,7 +6,6 @@ export const emojiGroupApi = async (group: keyof EmojiAsyncGroup) => {
   const response = await axiosInstance
     .get<APIResponse<FetchedEmojiGroup>>(`/emojis?group=${group}`)
     .then((res) => {
-      console.log(res);
       if (res.statusText != "OK") {
         throw new Error("There was an error fetching emojis.");
       }
