4d83dc6057d58d91c2e12471bec16564b43b1743
Morfusee <markvalenzuela.coding@gmail.com>
2025-12-01 00:43:19 +0800

    feat: add models coming from Node.JS app

---
+++ backend_asp/Models/Changelog.cs
@@ -0,0 +1,38 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend_asp.Models;
+
+public class Changelog
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("name")]
+    public string? Name { get; set; }
+
+    [BsonElement("project")]
+    [BsonRepresentation(BsonType.ObjectId)]
+    [Required]
+    public string ProjectId { get; set; } = string.Empty;
+
+    [BsonElement("data")]
+    [Required]
+    public string Data { get; set; } = string.Empty;
+
+    [BsonElement("currentVersion")]
+    [Required]
+    public bool CurrentVersion { get; set; }
+
+    [BsonElement("members")]
+    public List<User> Members { get; set; } = [];
+
+    [BsonElement("createdAt")]
+    [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [BsonIgnore]
+    public Project? Project { get; set; }
+}
---
+++ backend_asp/Models/User.cs
@@ -0,0 +1,46 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend_asp.Models;
+
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+
+    [BsonElement("username")]
+    [Required]
+    public string Username { get; set; } = string.Empty;
+
+    [BsonElement("email")]
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [BsonElement("displayName")]
+    public string? DisplayName { get; set; }
+
+    [BsonElement("profilePicture")]
+    public string? ProfilePicture { get; set; }
+
+    [BsonElement("ownedProjects")]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public List<string> OwnedProjects { get; set; } = [];
+
+    [BsonElement("sharedProjects")]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public List<string> SharedProjects { get; set; } = [];
+
+    [BsonElement("createdAt")]
+    [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [BsonElement("updatedAt")]
+    [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    [BsonIgnore]
+    public List<Project> Projects { get; set; } = [];
+}
