16cc369a59f5c56e6bf33f9bf4ce2c6f2bea3b81
Morfusee <markvalenzuela.coding@gmail.com>
2025-12-01 01:23:27 +0800

    feat: add configuration registering

--- backend_asp/Program.cs
+++ backend_asp/Program.cs
@@ -1,4 +1,7 @@
+using backend_asp.Config;
+using backend_asp.Services;
 using DotNetEnv;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Load environment variables from .env file
@@ -7,6 +10,13 @@ builder.Configuration.AddEnvironmentVariables();
 
 // Add services to the container.
 
+// Configure MongoDbSettings
+builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDbSettings"));
+
+// Register MongoDbContext
+builder.Services.AddSingleton<MongoDbContext>();
+
+// Add controllers
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
