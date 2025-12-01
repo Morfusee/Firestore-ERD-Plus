d661aac2e4c4adc39bdcdda866548141d087b9b7
Morfusee <markvalenzuela.coding@gmail.com>
2025-12-02 03:20:11 +0800

    feat: implement abstraction and dynamic registration of services using AttributeServiceExtension

---
+++ backend_asp/Common/Attributes/TransientServiceAttribute.cs
@@ -0,0 +1,4 @@
+namespace backend_asp.Common.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+public sealed class TransientServiceAttribute : Attribute { }
