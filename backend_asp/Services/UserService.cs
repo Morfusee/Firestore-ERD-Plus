6236e4a9be605f6faf6cdb469b5304d8acd30ab2
Morfusee <markvalenzuela.coding@gmail.com>
2025-12-03 00:22:50 +0800

    update: change userservice to return dtos instead of model

--- backend_asp/Services/UserService.cs
+++ backend_asp/Services/UserService.cs
@@ -1,4 +1,6 @@
 using backend_asp.Common.Attributes;
+using backend_asp.DTOs.User;
+using backend_asp.Mappers;
 using backend_asp.Models;
 using backend_asp.Services.Interfaces;
 using MongoDB.Driver;
@@ -6,27 +8,31 @@ using MongoDB.Driver;
 namespace backend_asp.Services;
 
 [ScopedService]
-public class UserService(MongoDbContext context) : IUserService
+public class UserService(MongoDbContext context, UserMapper mapper) : IUserService
 {
     private readonly MongoDbContext _context = context;
+    private readonly UserMapper _mapper = mapper;
 
-    public async Task<List<User>> GetAllUsersAsync()
+    public async Task<List<UserResponseDto>> GetAllUsersAsync()
     {
-        return await _context.Users.Find(_ => true).ToListAsync();
+        return await _context.Users
+            .Find(_ => true).Project(user => _mapper.ToDto(user)).ToListAsync();
     }
 
-    public async Task<User?> GetUserByIdAsync(string id)
+    public async Task<UserResponseDto?> GetUserByIdAsync(string id)
     {
-        return await _context.Users.Find(user => user.Id == id).FirstOrDefaultAsync();
+        var user = await _context.Users.Find(user => user.Id == id).FirstOrDefaultAsync();
+        return user == null ? null : _mapper.ToDto(user);
     }
 
-    public async Task<User> CreateUserAsync(User user)
+    public async Task<UserResponseDto> CreateUserAsync(CreateUserDto user)
     {
-        await _context.Users.InsertOneAsync(user);
-        return user;
+        var newUser = _mapper.ToUser(user);
+        await _context.Users.InsertOneAsync(newUser);
+        return _mapper.ToDto(newUser);
     }
 
-    public async Task<User?> UpdateUserAsync(string id, User updatedUser)
+    public async Task<UserResponseDto?> UpdateUserAsync(string id, UpdateUserDto updatedUser)
     {
         var existingUser = await GetUserByIdAsync(id);
 
@@ -35,11 +41,10 @@ public class UserService(MongoDbContext context) : IUserService
             return null;
         }
 
-        updatedUser.Id = existingUser.Id;
-        updatedUser.UpdatedAt = DateTime.UtcNow;
+        _mapper.UpdateUser(updatedUser, existingUser);
 
-        var result = await _context.Users.ReplaceOneAsync(user => user.Id == id, updatedUser);
-        return result.IsAcknowledged && result.ModifiedCount > 0 ? updatedUser : null;
+        var result = await _context.Users.ReplaceOneAsync(user => user.Id == id, _mapper.ToUser(existingUser));
+        return result.IsAcknowledged ? _mapper.ToDto(_mapper.ToUser(existingUser)) : null;
     }
 
     public async Task<bool> DeleteUserAsync(string id)
