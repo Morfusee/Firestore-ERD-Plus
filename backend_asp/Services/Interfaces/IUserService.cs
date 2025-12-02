5f385f701ab2bf2d21a4e16b2057eb85e474d162
Morfusee <markvalenzuela.coding@gmail.com>
2025-12-03 00:22:12 +0800

    update: change UserService contract to use dtos

--- backend_asp/Services/Interfaces/IUserService.cs
+++ backend_asp/Services/Interfaces/IUserService.cs
@@ -1,13 +1,14 @@
 using backend_asp.Common.Attributes;
+using backend_asp.DTOs.User;
 using backend_asp.Models;
 
 namespace backend_asp.Services.Interfaces;
 
 public interface IUserService
 {
-    Task<List<User>> GetAllUsersAsync();
-    Task<User?> GetUserByIdAsync(string id);
-    Task<User> CreateUserAsync(User user);
-    Task<User?> UpdateUserAsync(string id, User updatedUser);
+    Task<List<UserResponseDto>> GetAllUsersAsync();
+    Task<UserResponseDto?> GetUserByIdAsync(string id);
+    Task<UserResponseDto> CreateUserAsync(CreateUserDto user);
+    Task<UserResponseDto?> UpdateUserAsync(string id, UpdateUserDto updatedUser);
     Task<bool> DeleteUserAsync(string id);
 }
