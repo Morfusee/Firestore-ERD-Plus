e65e300d871ecb01e61614857bcf13cc25a12e62
Morfusee <markvalenzuela.coding@gmail.com>
2025-12-04 12:04:54 +0800

    update: update UserService contract

--- backend/Services/Interfaces/IUserService.cs
+++ backend/Services/Interfaces/IUserService.cs
@@ -6,8 +6,8 @@ namespace backend.Services.Interfaces;
 public interface IUserService
 {
     Task<Result<IEnumerable<UserResponseDto>>> GetAllUsersAsync();
-    Task<Result<UserResponseDto?>> GetUserByIdAsync(string id);
+    Task<Result<UserResponseDto>> GetUserByIdAsync(string id);
     Task<Result<UserResponseDto>> CreateUserAsync(CreateUserDto user);
-    Task<Result<UserResponseDto?>> UpdateUserAsync(string id, UpdateUserDto updatedUser);
+    Task<Result<UserResponseDto>> UpdateUserAsync(string id, UpdateUserDto updatedUser);
     Task<Result<bool>> DeleteUserAsync(string id);
 }
