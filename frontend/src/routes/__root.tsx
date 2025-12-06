9cfcc5ab7e974889b7e7613934edd9d3e6a00b8a
Morfusee <markvalenzuela.coding@gmail.com>
2025-12-07 04:38:18 +0800

    feat: initialize routes

---
+++ frontend/src/routes/__root.tsx
@@ -0,0 +1,27 @@
+import { client } from "@/integrations/api/generated/client.gen";
+import { Loader } from "@mantine/core";
+import { QueryClient } from "@tanstack/react-query";
+import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+
+import TanStackQueryLayout from "../integrations/tanstack-query/layout";
+
+interface RouterContext {
+  queryClient: QueryClient;
+}
+
+export const Route = createRootRouteWithContext<RouterContext>()({
+  component: () => (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools />
+      <TanStackQueryLayout />
+    </>
+  ),
+  beforeLoad: () => {
+    client.setConfig({
+      baseUrl: import.meta.env.VITE_API_URL,
+    });
+  },
+  pendingComponent: Loader,
+});
