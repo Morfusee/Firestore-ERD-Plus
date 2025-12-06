5555c5d03d71dfbda562d9c294e81e7af6fa4adb
Morfusee <markvalenzuela.coding@gmail.com>
2025-12-07 06:31:57 +0800

    feat: add authentication guard to protected route
    Introduced authentication check in the /_protected route using useAuth. If the user is not authenticated, they are redirected to the login page; a loader is shown while authentication status is loading. The route now renders child routes via Outlet instead of a specific page.

--- frontend/src/routes/_protected/route.tsx
+++ frontend/src/routes/_protected/route.tsx
@@ -1,10 +1,25 @@
-import FirestoreERD from '@/pages/FirestoreERD'
-import { createFileRoute } from '@tanstack/react-router'
+import useAuth from "@/hooks/useAuth";
+import { Box, Loader } from "@mantine/core";
+import { createFileRoute, Navigate, Outlet } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/_protected')({
+export const Route = createFileRoute("/_protected")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <FirestoreERD />
+  // Check if the user is authenticated
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading)
+    return (
+      <Box className="w-screen h-screen grid place-content-center">
+        <Loader />
+      </Box>
+    );
+
+  if (!isAuthenticated) {
+    return <Navigate to={"/login"} />;
+  }
+
+  return <Outlet />;
 }
