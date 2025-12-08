64dc006f6ac59e5644beab7c76588edda69ea6de
Morfusee <markvalenzuela.coding@gmail.com>
2025-12-08 22:39:05 +0800

    feat: add endpoint to get user by email
    Introduces a new GET /email/{email} endpoint in UsersController to retrieve user details by email address. Updates IUserService interface to include GetUserByEmailAsync method.

--- backend/Services/UserService/IUserService.cs
+++ backend/Services/UserService/IUserService.cs
@@ -7,6 +7,7 @@ public interface IUserService
 {
     Task<Result<IEnumerable<UserResponseDto>>> GetAllUsersAsync();
     Task<Result<UserResponseDto>> GetUserByIdAsync(string id);
+    Task<Result<UserResponseDto>> GetUserByEmailAsync(string email);
     Task<Result<UserResponseDto>> CreateUserAsync(CreateUserDto user);
     Task<Result<UserResponseDto>> UpdateUserAsync(string id, UpdateUserDto updatedUser);
     Task<Result<bool>> DeleteUserAsync(string id);
