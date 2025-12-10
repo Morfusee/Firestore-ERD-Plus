e5d7db960b08210c2eea342d950d715a9c73ec39
Morfusee <markvalenzuela.coding@gmail.com>
2025-12-11 00:14:45 +0800

    feat: consume useUserSettings to initialize color scheme

--- frontend/src/integrations/mantine/mantine-context-provider.tsx
+++ frontend/src/integrations/mantine/mantine-context-provider.tsx
@@ -1,4 +1,9 @@
-import { MantineProvider, useMantineColorScheme } from "@mantine/core";
+import useUserSettings from "@/hooks/useUserSettings";
+import {
+  MantineColorScheme,
+  MantineProvider,
+  useMantineColorScheme,
+} from "@mantine/core";
 import { createContext, useContext, useEffect, useState } from "react";
 import createMantineTheme from "./mantine-theme";
 
@@ -9,21 +14,33 @@ interface ReactiveThemeContextValue {
   >;
 }
 
-const ReactiveThemeContext = createContext<ReactiveThemeContextValue | undefined>(undefined);
+const ReactiveThemeContext = createContext<
+  ReactiveThemeContextValue | undefined
+>(undefined);
 
 export function useReactiveTheme() {
   const ctx = useContext(ReactiveThemeContext);
   if (!ctx)
-    throw new Error("useReactiveTheme must be used within a ReactiveThemeProvider");
+    throw new Error(
+      "useReactiveTheme must be used within a ReactiveThemeProvider"
+    );
   return ctx;
 }
 
-export function ReactiveThemeProvider({ children }: { children: React.ReactNode }) {
+export function ReactiveThemeProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const [currentTheme, updateTheme] = useState(createMantineTheme(false));
+  const { settings } = useUserSettings()
 
   return (
     <ReactiveThemeContext.Provider value={{ currentTheme, updateTheme }}>
-      <MantineProvider theme={currentTheme} defaultColorScheme="dark">
+      <MantineProvider
+        theme={currentTheme}
+        defaultColorScheme={settings.theme.toLowerCase() as MantineColorScheme}
+      >
         <ThemeSyncWrapper>{children}</ThemeSyncWrapper>
       </MantineProvider>
     </ReactiveThemeContext.Provider>
