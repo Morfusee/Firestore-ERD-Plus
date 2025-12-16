43d12fc5c361638e0c8835486ead3872f2c9fee9
Morfusee <markvalenzuela.coding@gmail.com>
2025-12-16 11:15:58 +0800

    WIP: Implement Methods in the IProjectService Contract

---
+++ backend/Services/ProjectService/ProjectService.cs
@@ -0,0 +1,91 @@
+using backend.Common.Attributes;
+using backend.DTOs.Common;
+using backend.DTOs.Project;
+using backend.Mappers;
+using backend.Models;
+using FluentResults;
+using MongoDB.Driver;
+
+namespace backend.Services.ProjectService;
+
+[ScopedService]
+public class ProjectService(MongoDbContext context, ProjectMapper mapper) : IProjectService
+{
+    private readonly MongoDbContext _context = context;
+    private readonly ProjectMapper _mapper = mapper;
+
+    public async Task<Result<IEnumerable<ProjectResponseDto>>> GetAllProjectsAsync()
+    {
+        try
+        {
+            var project = await _context.Projects.Find(_ => true).ToListAsync();
+            var projectDtos = project.ConvertAll(proj => _mapper.ToDto(proj));
+            return Result.Ok<IEnumerable<ProjectResponseDto>>(projectDtos);
+        }
+        catch (Exception ex)
+        {
+            return Result
+                .Fail<IEnumerable<ProjectResponseDto>>("Failed to retrieve projects")
+                .WithError(ex.Message);
+        }
+    }
+
+    public async Task<Result<ProjectResponseDto>> CreateProjectAsync(
+        CreateProjectDto createProjectDto
+    )
+    {
+        try
+        {
+            var user = await _context
+                .Users.Find(u =>
+                    u.Email.Equals(
+                        createProjectDto.Email,
+                        StringComparison.CurrentCultureIgnoreCase
+                    )
+                )
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+                return Result.Fail<ProjectResponseDto>("User not found");
+            }
+
+            var project = _mapper.ToProject(createProjectDto, user.Id);
+
+            await _context.Projects.InsertOneAsync(project);
+
+            return Result.Ok(_mapper.ToDto(project));
+        }
+        catch (Exception ex)
+        {
+            return Result
+                .Fail<ProjectResponseDto>("Failed to create project")
+                .WithError(ex.Message);
+        }
+    }
+
+    public Task<Result<bool>> DeleteProjectAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result<ProjectResponseDto>> GetProjectByIdAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result<ProjectResponseDto>> GetProjectsByEmailAsync(EmailDto emailDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result<ProjectResponseDto>> SaveProjectAsync(SaveProjectDto saveProjectDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result<ProjectResponseDto>> UpdateProjectAsync(UpdateProjectDto updateProjectDto)
+    {
+        throw new NotImplementedException();
+    }
+}
