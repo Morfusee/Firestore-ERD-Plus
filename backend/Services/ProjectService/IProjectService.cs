a8e83bb029886a68031431f51a832046843aacde
Morfusee <markvalenzuela.coding@gmail.com>
2025-12-16 11:15:36 +0800

    WIP: Create Interface/Contract for ProjectService

---
+++ backend/Services/ProjectService/IProjectService.cs
@@ -0,0 +1,16 @@
+using backend.DTOs.Common;
+using backend.DTOs.Project;
+using FluentResults;
+
+namespace backend.Services.ProjectService;
+
+public interface IProjectService
+{
+    Task<Result<IEnumerable<ProjectResponseDto>>> GetAllProjectsAsync();
+    Task<Result<ProjectResponseDto>> GetProjectsByEmailAsync(EmailDto emailDto);
+    Task<Result<ProjectResponseDto>> GetProjectByIdAsync(string id);
+    Task<Result<ProjectResponseDto>> CreateProjectAsync(CreateProjectDto createProjectDto);
+    Task<Result<ProjectResponseDto>> UpdateProjectAsync(UpdateProjectDto updateProjectDto);
+    Task<Result<ProjectResponseDto>> SaveProjectAsync(SaveProjectDto saveProjectDto);
+    Task<Result<bool>> DeleteProjectAsync(string id);
+}
