90a37dcc6ae7bcf06f0451aed36ef5141c2bfff5
Morfusee <markvalenzuela.coding@gmail.com>
2025-12-23 11:06:36 +0800

    feat: add emojis to mongodbcontext's collection

--- backend/Services/MongoDbContext.cs
+++ backend/Services/MongoDbContext.cs
@@ -1,8 +1,8 @@
-using MongoDB.Driver;
+using backend.Common.Attributes;
 using backend.Config;
 using backend.Models;
-using backend.Common.Attributes;
 using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 
 namespace backend.Services;
 
@@ -20,7 +20,10 @@ public class MongoDbContext
     public IMongoCollection<User> Users => _database.GetCollection<User>("users");
     public IMongoCollection<Project> Projects => _database.GetCollection<Project>("projects");
     public IMongoCollection<Settings> Settings => _database.GetCollection<Settings>("settings");
-    public IMongoCollection<Changelog> Changelogs => _database.GetCollection<Changelog>("changelogs");
-    public IMongoCollection<Models.Version> Versions => _database.GetCollection<Models.Version>("versions");
+    public IMongoCollection<Changelog> Changelogs =>
+        _database.GetCollection<Changelog>("changelogs");
+    public IMongoCollection<Models.Version> Versions =>
+        _database.GetCollection<Models.Version>("versions");
     public IMongoCollection<History> Histories => _database.GetCollection<History>("histories");
+    public IMongoCollection<Emoji> Emojis => _database.GetCollection<Emoji>("emojis");
 }
