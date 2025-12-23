76c0ae10721ebd9a41410ff564bfd1d8483409c6
Morfusee <markvalenzuela.coding@gmail.com>
2025-12-23 11:07:50 +0800

    feat: consume EmojiService to serve requests from users

---
+++ backend/Controllers/EmojisController.cs
@@ -0,0 +1,42 @@
+using backend.Common.Extensions;
+using backend.Common.Models;
+using backend.DTOs.Emoji;
+using backend.Services.EmojiService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EmojisController(IEmojiService emojiService, ILogger<EmojisController> logger)
+    : ControllerBase
+{
+    private readonly IEmojiService _emojiService = emojiService;
+    private readonly ILogger<EmojisController> _logger = logger;
+
+    [HttpGet]
+    public async Task<ActionResult<ApiResponse<IEnumerable<EmojiResponseDto>>>> GetAllEmojis()
+    {
+        var emojis = await _emojiService.GetAllEmojisAsync();
+
+        return this.ToApiResponse(emojis);
+    }
+
+    [HttpGet("{hexcode}")]
+    public async Task<ActionResult<ApiResponse<EmojiResponseDto?>>> GetEmojiByHexcode(
+        [FromRoute(Name = "hexcode")] string hexcode
+    )
+    {
+        var emoji = await _emojiService.GetEmojiByHexcodeAsync(hexcode);
+
+        return this.ToApiResponse(emoji);
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult<ApiResponse<bool>>> DeleteAllEmojis()
+    {
+        var result = await _emojiService.DeleteAllEmojisAsync();
+
+        return this.ToApiResponse(result);
+    }
+}
