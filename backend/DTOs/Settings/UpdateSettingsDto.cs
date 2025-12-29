211c2e7d1fdb720593f921757699013b2407ef04
Morfusee <markvalenzuela.coding@gmail.com>
2025-12-29 14:45:51 +0800

    feat: add validation to AutoSaveInterval in UpdateSettingsDto
    Introduced a [Range] attribute to enforce that AutoSaveInterval is between 0 and 50, providing validation and an error message for invalid values.

--- backend/DTOs/Settings/UpdateSettingsDto.cs
+++ backend/DTOs/Settings/UpdateSettingsDto.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using backend.DTOs.Common;
 using backend.Models;
 
@@ -5,6 +6,7 @@ namespace backend.DTOs.Settings;
 
 public class UpdateSettingsDto : EmailDto
 {
+    [Range(0, 50, ErrorMessage = "AutoSaveInterval must be between 0 and 50.")]
     public int? AutoSaveInterval { get; set; }
 
     public CanvasBackgroundOptions? CanvasBackground { get; set; }
